b7141831542515320c34625bc04ff50e98256d94
Chris Johnson <Johnny.C.Alexander@gmail.com>
2021-02-13 23:47:05 -0600

    getting somewhere. Giphy works perfectly. Slack api coming along

--- src/Content.js
+++ src/Content.js
@@ -2,22 +2,35 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 const key = process.env.REACT_APP_GIPHYKEY;
+const request = `https://api.giphy.com/v1/gifs/random?api_key=${key}&tag=blazeit&rating=pg-13&limit=1`;
+const token = process.env.REACT_APP_VERIFICATION_TOKEN;
 
 class Content extends Component {
+  state = {
+    gif: [],
+  };
   componentDidMount() {
+    //this needs to be a function triggered by 420pm.
+    axios.get(request).then((res) => {
+      this.setState({
+        gif: res.data.data.image_url, //grabbing gif url, setting it to gif state
+      });
+    });
     axios
-      .get(
-        `https://api.giphy.com/v1/gifs/random?api_key=${key}&tag=420&rating=pg-13`
-      )
+      .post(`https://slack.com/api/conversations.list`, token)
       .then((res) => {
-        console.log('in response', res.data);
+        console.log('in slack call', res);
       })
       .catch((error) => {
         console.log(error);
       });
   }
   render() {
-    return <div></div>;
+    return (
+      <div>
+        <img src={this.state.gif} />
+      </div>
+    );
   }
 }
 
